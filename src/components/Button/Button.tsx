bc47a5d0ab1ee6359bc9a41e7b915220b1e7919b
Polina <polina.solodkova@gmail.com>
2025-09-29 15:59:54 +0300

    feat: create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Button.css";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+  return <button className="defButton" onClick={onClick}>{children}</button>;
+};
+
+export default Button;
